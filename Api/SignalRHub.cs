798cc800b26e23ae301426b2abb0d6ad5f6be834
tomkarho <tommi@tomkarho.com>
2018-05-11 16:44:13 +0300

    Simple messaging demo

---
+++ Api/SignalRHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Api
+{
+    public class SignalRHub: Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
